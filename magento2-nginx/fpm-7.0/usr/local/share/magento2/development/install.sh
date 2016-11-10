e9993b30185540fad3df23e7c2810362b6fc36f0
Kieren Evans <kevans@inviqa.com>
2016-11-10 10:29:59 +0000

    Import magento2 install script changes from Mow Direct

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install.sh
@@ -38,8 +38,12 @@ export DATABASE_HOST=database
 sh "$DIR/install_database.sh"
 
 echo "DELETE from core_config_data WHERE path LIKE 'web/%base_url';
+DELETE from core_config_data WHERE path LIKE 'system/full_page_cache/varnish%';
 INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'web/unsecure/base_url', '$PUBLIC_ADDRESS');
-INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'web/secure/base_url', '$PUBLIC_ADDRESS');" |  mysql -h$DATABASE_HOST -u$DATABASE_USER -p$DATABASE_PASSWORD $DATABASE_NAME || exit 1
+INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'web/secure/base_url', '$PUBLIC_ADDRESS');
+INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'system/full_page_cache/varnish/access_list', 'varnish');
+INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'system/full_page_cache/varnish/backend_host', 'varnish');
+INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'system/full_page_cache/varnish/backend_port', '80');" |  mysql -h$DATABASE_HOST -u$DATABASE_USER -p$DATABASE_PASSWORD $DATABASE_NAME || exit 1
 
 sh "$DIR/install_assets.sh"
 
