7987e2718843531bc7de41723da3384719b3905d
Kieren Evans <kevans@inviqa.com>
2016-11-14 15:03:05 +0000

    Fix permissions issues that have been plaguing the magento2 build script.

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/install_magento.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/install_magento.sh
@@ -27,14 +27,23 @@ if [ ! -d "/app/vendor" ] || [ ! -f "/app/vendor/autoload.php" ]; then
   as_build "composer install --no-interaction"
   as_build "composer clear-cache"
 
+  chmod 600 auth.json
   chmod -R go-w vendor
-  chown -R www-data:www-data app pub var auth.json
   chmod +x bin/magento
+  chown -R www-data:www-data pub/media pub/static var
 fi
 
 if [ -d "/app/tools/inviqa" ]; then
+  if [ -d "/app/pub/static/frontend/" ]; then
+    chown -R build:build /app/pub/static/frontend/
+  fi
+
   if [ ! -d "/app/tools/inviqa/node_modules" ]; then
    as_build "npm install" "/app/tools/inviqa"
   fi
   as_build "gulp build" "/app/tools/inviqa"
+
+  if [ -d "/app/pub/static/frontend/" ]; then
+    chown -R www-data:www-data /app/pub/static/frontend/
+  fi
 fi
