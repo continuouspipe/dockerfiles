0710f1d95c2d72566e1608c739a981461f2b785d
Kieren Evans <kevans@inviqa.com>
2016-11-18 12:28:57 +0000

    Allow creation of core to fail. Curl sometimes returns 1 when it was a 200 response...

--- solr/4.10/usr/local/share/solr/startup.sh
+++ solr/4.10/usr/local/share/solr/startup.sh
@@ -29,7 +29,6 @@ rm /tmp/solr.log
 # Check for existing core
 curl -s "http://localhost:8983/solr/admin/cores?action=STATUS&core=$SOLR_CORE_NAME" | grep -q "<str name=\"name\">$SOLR_CORE_NAME</str>"
 CORE_EXISTS=$?
-set -e
 if [ "$CORE_EXISTS" -ne 0 ]; then
   curl -IX GET "http://localhost:8983/solr/admin/cores?action=CREATE&name=d8&instanceDir=$SOLR_CORE_NAME&config=solrconfig.xml&schema=schema.xml&dataDir=data"
 fi
