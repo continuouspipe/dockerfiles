a78a72383d726947346d511dbd58126a18b567bc
Kieren Evans <kevans@inviqa.com>
2016-11-22 11:55:32 +0000

    Force pulling of external images before building.

--- build.sh
+++ build.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 
-pushd ubuntu/16.04
-docker build -t quay.io/continuouspipe/ubuntu:16.04 .
-popd
+if [ -L "$0" ] ; then
+    DIR="$(dirname "$(readlink -f "$0")")" ;
+else
+    DIR="$(dirname "$0")" ;
+fi ;
 
-pushd php-apache/7.0
-docker build -t quay.io/continuouspipe/php-apache:7.0 .
-popd
-
-pushd ez/6.x
-docker build -t quay.io/continuouspipe/ez:6.x .
-popd
-
-pushd drupal8-apache/7.0
-docker build -t quay.io/continuouspipe/drupal8-apache:7.0 .
-popd
-
-pushd mysql/5.6
-docker build -t quay.io/continuouspipe/mysql:5.6 .
-popd
-
-pushd redis/3.2
-docker build -t quay.io/continuouspipe/redis:3.2 .
-popd
-
-pushd solr/6.2
-docker build -t quay.io/continuouspipe/solr:6.2 .
-popd
-
-pushd drupal8-solr/6.2
-docker build -t quay.io/continuouspipe/drupal8-solr:6.2 .
-popd
+(cd "$DIR" && docker-compose pull)
+(cd "$DIR" && docker-compose build --force-rm)
