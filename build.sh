ff0b1b9bec3da3c82b981611221e1ad1ed756255
Kieren Evans <kevans@inviqa.com>
2016-11-22 15:31:53 +0000

    Add option to push images after building.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 if [ -L "$0" ] ; then
     DIR="$(dirname "$(readlink -f "$0")")" ;
 else
@@ -10,3 +12,19 @@ echo "Pulling any external images:"; echo
 (cd "$DIR" && grep 'external_.*:' "$DIR/docker-compose.yml" | cut -d":" -f1 | xargs docker-compose pull)
 echo "Building all images:"; echo
 (cd "$DIR" && docker-compose build --force-rm)
+
+read -r -p "Would you like to publish the images? [Y/n] " DO_PUBLISH
+
+if [ -z "$DO_PUBLISH" ]; then
+  DO_PUBLISH='y'
+fi
+
+DO_PUBLISH="$(echo $DO_PUBLISH | tr '[A-Z]' '[a-z]')"
+if [ "$DO_PUBLISH" = 'y' ]; then
+  echo "Pushing our images:"; echo
+  (cd "$DIR" && docker-compose push)
+else
+  echo "Not Pushing our images."; echo
+fi
+
+echo "Done!"; echo
