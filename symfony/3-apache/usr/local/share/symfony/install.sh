e8e9dceca85cbe91ce1665c879bb092dd1ddc98b
Kieren Evans <kevans@inviqa.com>
2016-11-22 22:54:19 +0000

    Ensure writable permissions for /app/var are removed if present and not via NFS.

--- symfony/3-apache/usr/local/share/symfony/install.sh
+++ symfony/3-apache/usr/local/share/symfony/install.sh
@@ -14,9 +14,12 @@ set -e
 if [ "$IS_NFS" -ne 0 ]; then
   # Ensure code is owned by a user other than the web server user
   chown -R build:build /app
+  chmod -R go-rw /app/var
   # Fix permissions so the web server user can write to /app/var for symfony cache files
   setfacl -R -m u:www-data:rwX -m u:build:rwX /app/var
   setfacl -dR -m u:www-data:rwX -m u:build:rwX /app/var
+else
+  chmod -R a+rw /app/var
 fi
 
 if [ ! -d "/app/vendor" ] || [ ! -f "/app/vendor/autoload.php" ]; then
