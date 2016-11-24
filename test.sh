2d8037e20d54a11ed95703f9f65c3a6fc0251de9
Kieren Evans <kevans@inviqa.com>
2016-11-24 16:43:01 +0000

    Add file type filter.

--- test.sh
+++ test.sh
@@ -11,5 +11,5 @@ fi
 command -v shellcheck >/dev/null 2>&1 || { echo >&2 "I require shellcheck but it's not installed. Aborting."; exit 1; }
 command -v hadolint >/dev/null 2>&1 || { echo >&2 "I require hadolint but it's not installed. Aborting."; exit 1; }
 
-find "$DIR" \( -name "*.sh" -or -name "*_env_variables" -or -name "supervisor*_start" \) -exec shellcheck --exclude SC1091 {} +
-find "$DIR" -name "Dockerfile" -exec hadolint --ignore DL3008 --ignore DL3002 {} \;
+find "$DIR" -type f \( -name "*.sh" -or -name "*_env_variables" -or -name "supervisor*_start" \) -exec shellcheck --exclude SC1091 {} +
+find "$DIR" -type f -name "Dockerfile" -exec hadolint --ignore DL3008 --ignore DL3002 {} \;
