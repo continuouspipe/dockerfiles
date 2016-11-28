2c49d99315def24e4e4064873a7173e11a136648
Kieren Evans <kevans@inviqa.com>
2016-11-28 10:15:17 +0000

    Switch default local user to 'build', to make use of shared caches, instead of a random username/groupname.

--- ubuntu/16.04/usr/local/share/bootstrap/update_permissions.sh
+++ ubuntu/16.04/usr/local/share/bootstrap/update_permissions.sh
@@ -17,17 +17,21 @@ function update_permissions() {
     REFERENCE="$1"
     read -r owner group owner_id group_id < <(stat -c '%U %G %u %g' "$REFERENCE")
     if [[ "$owner" = UNKNOWN ]]; then
-        APP_USER="$(randname)"
+        APP_USER="build"
+        if [ "$APP_USER_LOCAL_RANDOM" == 'true' ]; then
+            APP_USER="$(randname)"
+        fi
         CODE_OWNER="$APP_USER"
         export APP_USER
         export CODE_OWNER
+
         if [[ "$group" = UNKNOWN ]]; then
             APP_GROUP="$owner"
             CODE_GROUP="$APP_GROUP"
             export APP_GROUP
             export CODE_GROUP
-            groupadd --system --gid "$group_id" "$APP_GROUP"
+            groupadd --system --gid "$group_id" "$APP_GROUP" || groupmod -g "$group_id" "$APP_GROUP"
         fi
-        useradd --create-home --system --uid "$owner_id" --gid "$group_id" "$APP_USER"
+        useradd --create-home --system --uid "$owner_id" --gid "$group_id" "$APP_USER" || usermod -u "$owner_id" "$APP_USER"
     fi
 }
