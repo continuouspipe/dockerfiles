227e5016b691279d539f257de9985354cbe89ea8
Kieren Evans <kevans@inviqa.com>
2016-11-30 16:34:12 +0000

    More tar flags to avoid failures.

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_assets.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_assets.sh
@@ -10,16 +10,16 @@ if [ -f /app/tools/assets/development/media.files.tgz ]; then
   IS_NFS=$?
   set -e
 
-  if [ "$IS_NFS" -eq 0 ]; then
+  if [ "$IS_NFS" -ne 0 ]; then
     chown -R "${CODE_OWNER}:${CODE_GROUP}" /app/pub/media
   else
     chmod -R a+rw /app/pub/media
   fi
 
   echo 'extracting media files'
-  as_code_owner "tar --no-same-owner --extract --strip-components=2 --gzip --file=/app/tools/assets/development/media.files.tgz || exit 1" /app/pub/media
+  as_code_owner "tar --no-same-owner --extract --strip-components=2 --touch --overwrite --gzip --file=/app/tools/assets/development/media.files.tgz || exit 1" /app/pub/media
 
-  if [ "$IS_NFS" -eq 0 ]; then
+  if [ "$IS_NFS" -ne 0 ]; then
     chown -R "${APP_USER}:${APP_GROUP}" /app/pub/media
     chmod -R u+rw,o-rw /app/media
   else
