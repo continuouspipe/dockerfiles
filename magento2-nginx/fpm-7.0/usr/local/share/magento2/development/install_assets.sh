50c5397b8fb171d1c20666702fcd8cbb02c69524
Kieren Evans <kevans@inviqa.com>
2016-11-30 16:09:22 +0000

    When extracting hem assets, fix permissions.

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_assets.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_assets.sh
@@ -4,7 +4,25 @@ set -xe
 
 source /usr/local/share/bootstrap/common_functions.sh
 
-if [ -f tools/assets/development/media.files.tgz ]; then
+if [ -f /app/tools/assets/development/media.files.tgz ]; then
+  set +e
+  is_nfs
+  IS_NFS=$?
+  set -e
+
+  if [ "$IS_NFS" -eq 0 ]; then
+    chown -R "${CODE_OWNER}:${CODE_GROUP}" /app/pub/media
+  else
+    chmod -R a+rw /app/pub/media
+  fi
+
   echo 'extracting media files'
-  as_code_owner "tar --no-same-owner --extract --gzip --file=tools/assets/development/media.files.tgz || exit 1"
+  as_code_owner "tar --no-same-owner --extract --strip-components=2 --gzip --file=/app/tools/assets/development/media.files.tgz || exit 1" /app/pub/media
+
+  if [ "$IS_NFS" -eq 0 ]; then
+    chown -R "${APP_USER}:${APP_GROUP}" /app/pub/media
+    chmod -R u+rw,o-rw /app/media
+  else
+    chmod -R a+rw /app/pub/media
+  fi
 fi
