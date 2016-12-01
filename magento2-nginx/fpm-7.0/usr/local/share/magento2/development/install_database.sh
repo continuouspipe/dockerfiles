373d71dc370361fb5f8de2db19af777a81ce6a0e
Kieren Evans <kevans@inviqa.com>
2016-12-01 10:29:40 +0000

    Check if the magento2 tables are present. If no tables, restore DB.

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_database.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_database.sh
@@ -6,12 +6,14 @@ if [ -f tools/assets/development/magentodb.sql.gz ]; then
     mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS $DATABASE_NAME" || exit 1
   fi
 
-  mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" -e "SHOW DATABASES" | grep -q magentodb
+  set +e
+  mysql -h"$DATABASE_HOST" -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" magentodb -e "SHOW TABLES; SELECT FOUND_ROWS() > 0;" | grep -q 1
   DATABASE_EXISTS=$?
+  set -e
 
   if [ "$DATABASE_EXISTS" -ne 0 ]; then
     echo 'Create Magento database'
-    echo "create database $DATABASE_NAME ; grant ALL on $DATABASE_NAME.* to $DATABASE_USER@'%' identified by '$DATABASE_PASSWORD' ; flush privileges" |  mysql -uroot -p"$DATABASE_ROOT_PASSWORD" -h"$DATABASE_HOST"
+    echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME ; GRANT ALL ON $DATABASE_NAME.* TO $DATABASE_USER@'%' IDENTIFIED BY '$DATABASE_PASSWORD' ; FLUSH PRIVILEGES" |  mysql -uroot -p"$DATABASE_ROOT_PASSWORD" -h"$DATABASE_HOST"
 
     echo 'zcating the magento database dump into the database'
     zcat tools/assets/development/magentodb.sql.gz | mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" "$DATABASE_NAME" || exit 1
