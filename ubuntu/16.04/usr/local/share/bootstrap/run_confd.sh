97064c2f24cbe1384cc5cc9f4fbf2d9e93cd104e
Kieren Evans <kevans@inviqa.com>
2016-12-01 23:39:41 +0000

    Extract setup of environment variables to setup.sh.

--- ubuntu/16.04/usr/local/share/bootstrap/run_confd.sh
+++ ubuntu/16.04/usr/local/share/bootstrap/run_confd.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Initialisation - Declare custom environment variables
-source /usr/local/share/env/custom_env_variables
+source /usr/local/share/bootstrap/setup.sh
 
-# Initialisation - Declare default environment variables
-source /usr/local/share/env/default_env_variables
-
-# Initialisation - Declare variables used by scripts in the ubuntu base image
-# to avoid undeclared variables!
-source /usr/local/share/env/bootstrap_env_variables
-
-# Initialisation - Pre templating
-source /usr/local/share/bootstrap/pre_templating.sh
-
-# Initialisation - create a user to match the mountpoint's settings if told to
-source /usr/local/share/bootstrap/trigger_update_permissions.sh
-
-# Initialisation - Templating
-confd -onetime -backend env
+do_confd
