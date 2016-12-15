69e5f312eba126dc1d92d37c7107c2d3f882921c
Kieren Evans <kevans@inviqa.com>
2016-12-15 12:44:15 +0000

    Move the settings.docker.php file into place.

--- drupal7-apache/5.6/usr/local/share/drupal7/install.sh
+++ drupal7-apache/5.6/usr/local/share/drupal7/install.sh
@@ -12,6 +12,10 @@ source /usr/local/share/php/common_functions.sh
 
 cd /app || exit 1;
 
+if [ -f "tools/docker/config/settings.docker.php" ]; then
+  cp tools/docker/config/settings.docker.php /app/docroot/sites/default/settings.docker.php
+fi
+
 run_composer
 
 if [ -f "$DIR/install_custom.sh" ]; then
