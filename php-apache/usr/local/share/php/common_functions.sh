3052ee9195cbc2808bdda3dc258ab63d16a22d6b
Kieren Evans <kevans@inviqa.com>
2017-01-13 11:27:33 +0000

    Replace use of setfacl and fix the NFS version to use chmod instead of chown.

--- php-apache/usr/local/share/php/common_functions.sh
+++ php-apache/usr/local/share/php/common_functions.sh
@@ -2,9 +2,9 @@
 
 do_build_permissions() {
   if [ "$IS_NFS" -ne 0 ]; then
-    setfacl -R -m "d:u:$CODE_OWNER:rwX" -m "u:$CODE_OWNER:rwX" /app/
+    chown -R "$CODE_OWNER":"$CODE_GROUP" /app/
   else
-    chown -R "$CODE_OWNER:$CODE_GROUP" /app/
+    chmod -R a+rw /app/
   fi
 }
 
