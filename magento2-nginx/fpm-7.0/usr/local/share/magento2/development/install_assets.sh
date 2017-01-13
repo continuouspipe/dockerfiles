50d8c208d811d22e2246997617da183a8d7c601c
Kieren Evans <kevans@inviqa.com>
2017-01-13 16:52:01 +0000

    Rename IS_NFS to IS_CHOWN_SUPPORTED, plus the associate function, to allow for vboxfs.

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_assets.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/development/install_assets.sh
@@ -9,11 +9,11 @@ cd /app || exit 1
 
 if [ -f "$ASSET_ARCHIVE_PATH" ]; then
   set +e
-  is_nfs
-  IS_NFS=$?
+  is_chown_supported
+  IS_CHOWN_SUPPORTED=$?
   set -e
 
-  if [ "$IS_NFS" -ne 0 ]; then
+  if [ "$IS_CHOWN_SUPPORTED" -ne 0 ]; then
     chown -R "${CODE_OWNER}:${CODE_GROUP}" pub/media
   else
     chmod -R a+rw pub/media
@@ -22,7 +22,7 @@ if [ -f "$ASSET_ARCHIVE_PATH" ]; then
   echo 'extracting media files'
   as_code_owner "tar --no-same-owner --extract --strip-components=2 --touch --overwrite --gzip --file=$ASSET_ARCHIVE_PATH || exit 1" pub/media
 
-  if [ "$IS_NFS" -ne 0 ]; then
+  if [ "$IS_CHOWN_SUPPORTED" -ne 0 ]; then
     chown -R "${APP_USER}:${APP_GROUP}" pub/media
     chmod -R u+rw,o-rw pub/media
   else
--- ubuntu/16.04/usr/local/share/bootstrap/common_functions.sh
+++ ubuntu/16.04/usr/local/share/bootstrap/common_functions.sh
@@ -46,7 +46,7 @@ is_hem_project() {
   return 1
 }
 
-is_nfs() {
+is_chown_supported() {
   # Determine if the app directory is an NFS mountpoint, which doesn't allow chowning.
   grep -q "/app nfs " /proc/mounts
   return $?
