4a938d0ee69b8ef4bb32dc4ecc8c283aa2667c0c
Andy Thompson <athompson@inviqa.com>
2017-01-17 12:10:59 +0000

    Add apache to symfony build.sh and commit Dockerfiles

--- symfony/build.sh
+++ symfony/build.sh
@@ -8,7 +8,7 @@ else
     DIR="$(dirname "$0")" ;
 fi ;
 
-for WEB_SERVER in nginx; do
+for WEB_SERVER in nginx apache; do
     for PHP_VERSION in 5.6 7 7.1; do
         sed "s/{{PHP_VERSION}}/$PHP_VERSION/g; s/{{WEB_SERVER}}/$WEB_SERVER/g;" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-php${PHP_VERSION}-${WEB_SERVER}"
     done
