03cffc2ae3c8ca88acbe433fc3043d6aeb4a89bf
Andy Thompson <athompson@inviqa.com>
2017-01-17 12:05:28 +0000

    Merge symfony-3-apache into symfony-nginx work
    Using template variables to control the base image and etc directory to use
    
    Note this changes the registry push location to match the format of the symfony-nginx repositories.
    
    It also brings in PHP 7.1 and 5.6 versions of Symfony for Apache

---
+++ symfony/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -xe
+
+if [ -L "$0" ] ; then
+    DIR="$(dirname "$(readlink -f "$0")")" ;
+else
+    DIR="$(dirname "$0")" ;
+fi ;
+
+for WEB_SERVER in nginx; do
+    for PHP_VERSION in 5.6 7 7.1; do
+        sed "s/{{PHP_VERSION}}/$PHP_VERSION/g; s/{{WEB_SERVER}}/$WEB_SERVER/g;" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-php${PHP_VERSION}-${WEB_SERVER}"
+    done
+done
