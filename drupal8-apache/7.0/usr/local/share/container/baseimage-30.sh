aba270e99263b34cf3b1073797aee75945f6853e
Matt Chapman <chapabu101@gmail.com>
2017-02-13 14:10:17 +0000

    Initial commit of Drupal 8 functions.

--- drupal8-apache/7.0/usr/local/share/container/baseimage-30.sh
+++ drupal8-apache/7.0/usr/local/share/container/baseimage-30.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
 
+source /usr/local/share/drupal8/drupal8_functions.sh
+
+#####
+# Tasks here happen during the bulding of the Dockerfile, and cannot rely on
+# other services being available.
+#####
 alias_function do_build do_drupal8_build_inner
 do_build() {
   do_drupal8_build_inner
-  do_drupal8_install
+  do_drupal8_build
 }
 
-alias_function do_development_start do_drupal8_development_start_inner
-do_development_start() {
-  do_drupal8_development_start_inner
-  do_drupal8_development_start
-}
-
-alias_function do_templating do_drupal8_templating_inner
-do_templating() {
-  mkdir -p /app/docroot/sites/default/
-  do_drupal8_templating_inner
-}
-
-do_composer() {
-  # disable original composer in image hierarchy till install ported
-  :
-}
-
-do_drupal8_install() {
-  bash /usr/local/share/drupal8/install.sh
-}
-
-do_drupal8_development_start() {
-  bash /usr/local/share/drupal8/development/install.sh
-  do_drupal8_setup
-}
-
+#####
+# Tasks here are executed once all other services are available, so it should
+# be safe to to any tasks requiring databases, SOLR, etc.
+#####
+alias_function do_setup do_drupal8_setup_inner
 do_setup() {
-  do_drupal8_setup
+  do_drupal8_setup_inner
 }
 
-do_drupal8_setup() {
-  bash /usr/local/share/drupal8/install_finalise.sh
+#####
+# Tasks here are run when the container is started, and all services should be
+# available.
+####
+alias_function do_start do_drupal8_start_inner
+do_start() {
+  do_drupal8_start_inner
 }
---
+++ drupal8-apache/7.0/usr/local/share/drupal8/drupal8_functions.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+
+#####
+# Perform the Drupal 8 build.
+#####
+do_drupal8_build() {
+  do_drupal8_create_directories()
+  do_drupal8_deck_build()
+  do_drupal8_permissions()
+}
+
+#####
+# Create any directories that aren't given by default.
+#####
+do_drupal8_create_directories() {
+  mkdir -p /app/sites/default/files
+}
+
+#####
+# Fix directory permissions.
+#####
+do_drupal8_permissions() {
+  if [ "$IS_CHOWN_FORBIDDEN" -ne 0 ]; then
+    # Give the docroot to the web user.
+    chown -R ${APP_USER}:${APP_GROUP} /app/docroot
+
+    # Ensure the files directory is writable.
+    chmod g+w /app/sites/default/files
+
+    # Setting.php needs to be writable during installation, but Drupal will fix
+    # this later.
+    chmod go+w /app/sites/default/settings.php
+  fi
+}
+
+#####
+# Build the front end assets, but only if the Deck tools exist.
+#
+# Todo: Make this a little more customisable for users who aren't using the
+# Deck theme tools.
+#####
+do_drupal8_deck_build() {
+  # If the Deck tools exist, then run them.
+  if [ -f /app/gulp/gulpfile.js ]; then
+    as_code_owner "npm install"
+    as_code_owner "node_modules/.bin/gulp build"
+  fi
+}
