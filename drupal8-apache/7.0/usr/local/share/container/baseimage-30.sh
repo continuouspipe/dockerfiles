ddf6fc7ec399683d619dd6c64506eb7cf5cbadb6
Matt Chapman <chapabu101@gmail.com>
2017-02-20 11:04:37 +0000

    Made functions more version-agnostic. Added site install.

--- drupal8-apache/7.0/usr/local/share/container/baseimage-30.sh
+++ drupal8-apache/7.0/usr/local/share/container/baseimage-30.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
-source /usr/local/share/drupal8/drupal8_functions.sh
+# This is intentionally left as drupal8_functions for the time being.
+# If we need a different set of functions for Drupal 7, then we can load in a
+# different functions file, and keep this main one the same.
+source /usr/local/share/drupal/drupal8_functions.sh
+
+set_drupal_version() {
+  drush core-status drupal-version --format=list | cut -d"." -f 1
+}
 
 #####
 # Tasks here happen during the bulding of the Dockerfile, and cannot rely on
 # other services being available.
 #####
-alias_function do_build do_drupal8_build_inner
+alias_function do_build do_drupal_build_inner
 do_build() {
-  do_drupal8_build_inner
-  do_drupal8_build
+  do_drupal_build_inner
+  do_drupal_build
 }
 
-alias_function do_build do_drupal8_development_build_inner
+alias_function do_build do_drupal_development_build_inner
 do_development_build() {
-  do_drupal8_development_build_inner
-  do_drupal8_development_build
+  do_drupal_development_build_inner
+  do_drupal_development_build
 }
 
 #####
 # Tasks here are executed once all other services are available, so it should
 # be safe to to any tasks requiring databases, SOLR, etc.
 #####
-# alias_function do_setup do_drupal8_setup_inner
+# alias_function do_setup do_drupal_setup_inner
 # do_setup() {
-#   do_drupal8_setup_inner
+#   do_drupal_setup_inner
 # }
 
 #####
 # Tasks here are run when the container is started, and all services should be
 # available.
 ####
-alias_function do_start do_drupal8_start_inner
+alias_function do_start do_drupal_start_inner
 do_start() {
-  do_drupal8_start_inner
-  do_drupal8_start
+  do_drupal_start_inner
+  do_drupal_start
 }
 
-alias_function do_development_start do_drupal8_development_start_inner
+alias_function do_development_start do_drupal_development_start_inner
 do_development_start() {
-  do_drupal8_development_start_inner
-  do_drupal8_development_start
+  do_drupal_development_start_inner
+  do_drupal_development_start
 }
---
+++ drupal8-apache/7.0/usr/local/share/drupal/drupal8_functions.sh
@@ -0,0 +1,129 @@
+#! /usr/bin/env bash
+
+#####
+# Perform the Drupal 8 build.
+#####
+do_drupal_build() {
+  do_drupal_create_directories
+  do_drupal_deck_build
+  do_drupal_permissions
+  do_drupal_legacy_install_script
+}
+
+do_drupal_development_build() {
+  do_drupal_build
+  do_drupal_legacy_development_install_script
+}
+
+#####
+# Perform tasks on container start
+#####
+do_drupal_start() {
+  do_drupal_permissions
+  do_drupal_legacy_install_finalise_script
+}
+
+do_drupal_development_start() {
+  do_drupal_start
+  do_drupal_development_install
+  do_drupal_legacy_development_install_script
+}
+
+#####
+# Create any directories that aren't given by default.
+#####
+do_drupal_create_directories() {
+  mkdir -p "${WEB_DIRECTORY}/sites/default/files"
+}
+
+#####
+# Fix directory permissions.
+#####
+do_drupal_permissions() {
+  if [ "$IS_CHOWN_FORBIDDEN" -ne 0 ]; then
+    # Give the docroot and config directories to the web user.
+    chown -R "${APP_USER}":"${APP_GROUP}" "${WEB_DIRECTORY}"
+
+    # Ensure the config directory is writeable by the group
+    if [ -d /app/config ]; then
+      chmod -R g+w /app/config
+      chown -R "${APP_USER}":"${APP_GROUP}" /app/config
+    fi
+
+    # Ensure the files directory is writable.
+    chmod -R g+w "${WEB_DIRECTORY}/sites/default/files"
+
+    # Setting.php needs to be writable during installation, but Drupal will fix
+    # this later.
+    chmod go+w "${WEB_DIRECTORY}/sites/default/settings.php"
+  fi
+}
+
+#####
+# Build the front end assets, but only if the Deck tools exist.
+#
+# Todo: Make this a little more customisable for users who aren't using the
+# Deck theme tools.
+#####
+do_drupal_deck_build() {
+  # If the Deck tools exist, then run them.
+  if [ -f /app/gulp/gulpfile.js ]; then
+    cd /app/gulp || return
+    as_code_owner "npm install"
+    as_code_owner "node_modules/.bin/gulp build"
+  fi
+}
+
+#####
+# Support the legacy install_custom.sh scripts.
+#####
+do_drupal_legacy_install_script() {
+  if [ -f /usr/local/share/drupal/install_custom.sh ]; then
+    bash /usr/local/share/drupal/install_custom.sh
+  fi
+}
+
+do_drupal_legacy_development_install_script() {
+  if [ -f /usr/local/share/drupal/development/install_custom.sh ]; then
+    bash /usr/local/share/drupal/development/install_custom.sh
+  fi
+}
+
+
+####
+# Support the legacy install_custom_finalise.sh scripts.
+####
+do_drupal_legacy_install_finalise_script() {
+  if [ -f /usr/local/share/drupal/install_finalise.sh ]; then
+    bash /usr/local/share/drupal/install_finalise.sh
+  fi
+}
+
+do_drupal_legacy_development_install_finalise_script() {
+  if [ -f /usr/local/share/drupal/development/install_finalise.sh ]; then
+    bash /usr/local/share/drupal/development/install_finalise.sh
+  fi
+}
+
+#####
+# Install Drupal if required.
+#####
+do_drupal_development_install() {
+
+  if [ "$INSTALL_DRUPAL" == 'false' ]; then
+    return
+  fi
+
+  # Drop the database if we need to force the install every time.
+  if [ "$FORCE_DATABASE_DROP" == 'true' ]; then
+    echo 'Dropping the Drupal DB if it exists'
+    "drush sql-drop -y -r ${WEB_DIRECTORY}"
+  fi
+
+  # If we're supposed to install Drupal, and it's not currently installed - then install it.
+  if [ ! drush status bootstrap | grep -q Successful ] ; then
+    echo 'Installing Drupal'
+    "drush site-install ${DRUPAL_INSTALL_PROFILE} -y -r ${WEB_DIRECTORY}"
+  fi
+
+}
