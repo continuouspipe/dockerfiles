d07f09279f0b8a184aaf1497a61fdf7ee8b9f1cf
Matt Chapman <chapabu101@gmail.com>
2017-02-21 17:19:27 +0000

    Moved sensitive permissions to development function.

--- drupal8-apache/7.0/usr/local/share/drupal/drupal8_functions.sh
+++ drupal8-apache/7.0/usr/local/share/drupal/drupal8_functions.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 #####
-# Perform the Drupal 8 build.
+# Perform the Drupal build.
 #####
 do_drupal_build() {
   do_drupal_create_directories
@@ -11,13 +11,13 @@ do_drupal_build() {
 
 do_drupal_development_build() {
   do_drupal_build
+  do_drupal_development_permissions
   do_drupal_legacy_development_install_script
 }
 
 #####
 # Perform setup tasks.
 #####
-
 do_drupal_setup() {
   do_drupal_install
 }
@@ -45,9 +45,6 @@ do_drupal_create_directories() {
 #####
 do_drupal_permissions() {
   if [ "$IS_CHOWN_FORBIDDEN" -ne 0 ]; then
-    # Give the docroot and config directories to the web user.
-    chown -R "${APP_USER}":"${APP_GROUP}" "${WEB_DIRECTORY}"
-
     # Ensure the config directory is writeable by the group
     if [ -d /app/config ]; then
       chmod -R g+w /app/config
@@ -56,11 +53,28 @@ do_drupal_permissions() {
 
     # Ensure the files directory is writable.
     chmod -R g+w "${WEB_DIRECTORY}/sites/default/files"
-
-    # Setting.php needs to be writable during installation, but Drupal will fix
-    # this later.
-    chmod go+w "${WEB_DIRECTORY}/sites/default/settings.php"
   fi
+}
+
+#####
+# Fix directory permissions for development.
+#####
+do_drupal_development_permissions() {
+    # If we're telling the system that we don't want to install Drupal, then
+    # chances are we want to install it through the UI. If that's the case,
+    # then the settings file needs to be writeable. This should be restricted
+    # to development mode, though.
+    if [ "$INSTALL_DRUPAL" == 'false' ]; then
+      chmod go+w "${WEB_DIRECTORY}/sites/default/settings.php"
+    fi
+
+    # We need to be able to write to the module directory for Feature
+    # generation.
+    chmod go+w "${WEB_DIRECTORY}/modules"
+
+    # We may need to be able to write to the profiles directory as well if
+    # we're creating an installation profile with Features.
+    chmod go+w "${WEB_DIRECTORY}/profiles"
 }
 
 #####
@@ -77,7 +91,6 @@ do_drupal_legacy_development_install_script() {
     bash /usr/local/share/drupal/development/install_custom.sh
   fi
 }
-
 
 ####
 # Support the legacy install_custom_finalise.sh scripts.
@@ -99,6 +112,7 @@ do_drupal_legacy_development_install_finalise_script() {
 #####
 do_drupal_install() {
 
+  # If we don't want Drupal installed, then return.
   if [ "$INSTALL_DRUPAL" == 'false' ]; then
     return
   fi
@@ -109,9 +123,13 @@ do_drupal_install() {
     as_code_owner "drush sql-drop -y -r ${WEB_DIRECTORY}"
   fi
 
-  # If we're supposed to install Drupal, and it's not currently installed - then install it.
+  # If we're supposed to install Drupal, and it's not currently installed,
+  # then install it.
   if ! drush status bootstrap | grep -q Successful ; then
     echo 'Installing Drupal'
+    # We should make sure this is writeable, but only do it directly before an
+    # install. Drupal will lock it back down on install completion.
+    chmod go+w "${WEB_DIRECTORY}/sites/default/settings.php"
     as_code_owner "drush site-install ${DRUPAL_INSTALL_PROFILE} -y -r ${WEB_DIRECTORY}"
   fi
 
