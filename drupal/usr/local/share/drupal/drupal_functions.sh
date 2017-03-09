8ab786772a839e0012bc0c48713a658222cec849
Kieren Evans <kevans@inviqa.com>
2017-03-09 13:49:40 +0000

    Provide a standard way to import a database into drupal.

--- drupal/usr/local/share/drupal/drupal_functions.sh
+++ drupal/usr/local/share/drupal/drupal_functions.sh
@@ -107,3 +107,31 @@ do_drupal_install() {
   fi
 
 }
+
+#####
+# Restore a database if required.
+# Not triggered by default, please call from a function in your plan.sh to use.
+#####
+do_drupal_database_install() {
+  set +x
+  if [ -f "$DATABASE_ARCHIVE_PATH" ]; then
+    if [ "$FORCE_DATABASE_DROP" == 'true' ]; then
+      echo 'Dropping the Drupal DB if it exists'
+      mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS $DATABASE_NAME" || exit 1
+    fi
+
+    set +e
+    mysql -h"$DATABASE_HOST" -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" "$DATABASE_NAME" -e "SHOW TABLES; SELECT FOUND_ROWS() > 0;" | grep -q 1
+    DATABASE_EXISTS=$?
+    set -e
+
+    if [ "$DATABASE_EXISTS" -ne 0 ]; then
+      echo 'Create Drupal database'
+      echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME ; GRANT ALL ON $DATABASE_NAME.* TO $DATABASE_USER@'%' IDENTIFIED BY '$DATABASE_PASSWORD' ; FLUSH PRIVILEGES" |  mysql -uroot -p"$DATABASE_ROOT_PASSWORD" -h"$DATABASE_HOST"
+
+      echo 'zcating the drupal database dump into the database'
+      zcat "$DATABASE_ARCHIVE_PATH" | mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" "$DATABASE_NAME" || exit 1
+    fi
+  fi
+  set -x
+}
