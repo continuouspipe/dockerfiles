a18ba2be072e4f44f7789fda03e25bcd6ed26f41
Hardik Gajjar <hgajjar@sessiondigital.de>
2017-03-21 13:54:08 +0100

    - Put install steps in proper order in Dockerfile - Update alias function order to let symfony_build command change the ownership before running spryker commands - Add spryker console commands

--- spryker/nginx/usr/local/share/container/baseimage-45.sh
+++ spryker/nginx/usr/local/share/container/baseimage-45.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /usr/local/share/spryker/spryker_functions.sh
 
-alias_function do_symfony_build do_spryker_symfony_build_inner
+alias_function do_build_permissions do_spryker_build_permissions_inner
 do_build_permissions() {
+  do_spryker_build_permissions_inner
   do_spryker_build
-  do_spryker_symfony_build_inner
 }
--- spryker/nginx/usr/local/share/spryker/spryker_functions.sh
+++ spryker/nginx/usr/local/share/spryker/spryker_functions.sh
@@ -9,11 +9,37 @@ do_spryker_directory_create() {
 
 do_spryker_config_create() {
   # create .pgpass in home directory for postgres client
-  echo "$DATABASE_HOST:*:*:$DATABASE_USER:$DATABASE_PASSWORD" > ~/.pgpass
-  chmod 0600 ~/.pgpass
+  as_code_owner "echo \"$DATABASE_HOST:*:*:$DATABASE_USER:$DATABASE_PASSWORD\" > ~/.pgpass"
+  as_code_owner "chmod 0600 ~/.pgpass"
 }
 
 do_spryker_build() {
   do_spryker_directory_create
   do_spryker_config_create
+}
+
+do_build_assets() {
+  as_code_owner "cd /app && antelope install"
+  as_code_owner "cd /app && antelope build zed"
+  as_code_owner "cd /app && antelope build yves"
+}
+
+do_database_update() {
+  as_code_owner "/app/vendor/bin/console setup:deploy:prepare-propel"
+  as_code_owner "/app/vendor/bin/console transfer:generate"
+  as_code_owner "/app/vendor/bin/console setup:search:index-map"
+  as_code_owner "/app/vendor/bin/console application:build-navigation-cache"
+}
+
+do_setup() {
+  do_build_assets
+  do_database_update
+}
+
+do_spryker_install() {
+  as_code_owner "/app/vendor/bin/console setup:install"
+  as_code_owner "/app/vendor/bin/console import:demo-data"
+  as_code_owner "/app/vendor/bin/console collector:search:export"
+  as_code_owner "/app/vendor/bin/console collector:storage:export"
+  as_code_owner "/app/vendor/bin/console setup:search"
 }
