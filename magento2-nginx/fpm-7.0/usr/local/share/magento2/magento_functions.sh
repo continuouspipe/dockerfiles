00a46f3a95b51c936b4096bd17f30c018bb7245b
Kieren Evans <kevans@inviqa.com>
2017-03-29 14:08:06 +0100

    Allow passwordless installs for during build process.

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/magento_functions.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/magento_functions.sh
@@ -131,20 +131,36 @@ function do_magento_database_install() {
   if [ -f "$DATABASE_ARCHIVE_PATH" ]; then
     if [ "$FORCE_DATABASE_DROP" == 'true' ]; then
       echo 'Dropping the Magento DB if exists'
-      mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS $DATABASE_NAME" || exit 1
+      if [ -n "$DATABASE_ROOT_PASSWORD" ]; then
+        mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" -e "DROP DATABASE IF EXISTS $DATABASE_NAME" || exit 1
+      else
+        mysql -h"$DATABASE_HOST" -uroot -e "DROP DATABASE IF EXISTS $DATABASE_NAME" || exit 1
+      fi
     fi
   
     set +e
-    mysql -h"$DATABASE_HOST" -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" "$DATABASE_NAME" -e "SHOW TABLES; SELECT FOUND_ROWS() > 0;" | grep -q 1
+    if [ -n "$DATABASE_PASSWORD" ]; then
+      mysql -h"$DATABASE_HOST" -u"$DATABASE_USER" -p"$DATABASE_PASSWORD" "$DATABASE_NAME" -e "SHOW TABLES; SELECT FOUND_ROWS() > 0;" | grep -q 1
+    else
+      mysql -h"$DATABASE_HOST" -u"$DATABASE_USER" "$DATABASE_NAME" -e "SHOW TABLES; SELECT FOUND_ROWS() > 0;" | grep -q 1
+    fi
     DATABASE_EXISTS=$?
     set -e
   
     if [ "$DATABASE_EXISTS" -ne 0 ]; then
       echo 'Create Magento database'
-      echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME ; GRANT ALL ON $DATABASE_NAME.* TO $DATABASE_USER@'%' IDENTIFIED BY '$DATABASE_PASSWORD' ; FLUSH PRIVILEGES" |  mysql -uroot -p"$DATABASE_ROOT_PASSWORD" -h"$DATABASE_HOST"
+      if [ -n "$DATABASE_ROOT_PASSWORD" ]; then
+        echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME ; GRANT ALL ON $DATABASE_NAME.* TO $DATABASE_USER@'%' IDENTIFIED BY '$DATABASE_PASSWORD' ; FLUSH PRIVILEGES" |  mysql -uroot -p"$DATABASE_ROOT_PASSWORD" -h"$DATABASE_HOST"
+      else
+        echo "CREATE DATABASE IF NOT EXISTS $DATABASE_NAME ; GRANT ALL ON $DATABASE_NAME.* TO $DATABASE_USER@'%' IDENTIFIED BY '$DATABASE_PASSWORD' ; FLUSH PRIVILEGES" |  mysql -uroot -h"$DATABASE_HOST"
+      fi
   
       echo 'zcating the magento database dump into the database'
-      zcat "$DATABASE_ARCHIVE_PATH" | mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" "$DATABASE_NAME" || exit 1
+      if [ -n "$DATABASE_ROOT_PASSWORD" ]; then
+        zcat "$DATABASE_ARCHIVE_PATH" | mysql -h"$DATABASE_HOST" -uroot -p"$DATABASE_ROOT_PASSWORD" "$DATABASE_NAME" || exit 1
+      else
+        zcat "$DATABASE_ARCHIVE_PATH" | mysql -h"$DATABASE_HOST" -uroot "$DATABASE_NAME" || exit 1
+      fi
     fi
   fi
   set -x
