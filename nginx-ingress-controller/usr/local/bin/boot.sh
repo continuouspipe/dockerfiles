b8db9257cd7fc15961469cf5102638c67886cbd3
Kieren Evans <kevans@inviqa.com>
2017-04-25 17:37:35 +0100

    Fix up shellcheck errors

--- nginx-ingress-controller/usr/local/bin/boot.sh
+++ nginx-ingress-controller/usr/local/bin/boot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "$(echo "$SSL_CERT" | base64 -d)" > /etc/nginx/default-certificate.pem
+echo "$SSL_CERT" | base64 -d > /etc/nginx/default-certificate.pem
 echo >> /etc/nginx/default-certificate.pem
-echo "$(echo "$SSL_KEY" | base64 -d)" >> /etc/nginx/default-certificate.pem
+echo "$SSL_KEY" | base64 -d >> /etc/nginx/default-certificate.pem
 
-export SSL_SHA1SUM="$(sha1sum /etc/nginx/default-certificate.pem)"
+SSL_SHA1SUM="$(sha1sum /etc/nginx/default-certificate.pem)"
+export SSL_SHA1SUM
 
-exec /nginx-ingress-controller $@
+exec /nginx-ingress-controller "$@"
