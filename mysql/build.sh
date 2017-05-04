66d5ef675ce729b5aa45cb798a01ac380cd7734c
Andy Thompson <athompson@inviqa.com>
2017-05-04 14:53:27 +0100

    Add MySQL 5.5 image for legacy projects
    MySQL 5.5 still supported by Oracle

--- mysql/build.sh
+++ mysql/build.sh
@@ -8,6 +8,6 @@ else
     DIR="$(dirname "$0")" ;
 fi ;
 
-for IMAGE_TAG in 5.6 5.7 8.0; do
+for IMAGE_TAG in 5.5 5.6 5.7 8.0; do
   sed "s/{{IMAGE_TAG}}/$IMAGE_TAG/g" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-${IMAGE_TAG}"
 done
