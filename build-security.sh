cf1e3fb480cffb2839b61628a3bc2833d5ec362e
Kieren Evans <kevans@inviqa.com>
2017-06-03 21:38:18 +0100

    Create a script to download all stable images, install security updates, and optionally push back up

---
+++ build-security.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -xe
+
+if [ -L "$0" ] ; then
+    DIR="$(dirname "$(readlink -f "$0")")" ;
+else
+    DIR="$(dirname "$0")" ;
+fi
+
+# Get list of images
+IMAGE_NAMES="$(grep "quay.io\/continuouspipe\/.*" "${DIR}/docker-compose.yml" | cut -d ":" -f2)"
+
+# Get list of image names without the organisation
+SHORT_IMAGE_NAMES="$(echo "$IMAGE_NAMES" | cut -d "/" -f3)"
+read -r -a SHORT_IMAGE_NAMES <<<$SHORT_IMAGE_NAMES
+read -r -a IMAGE_NAMES <<<$IMAGE_NAMES
+
+mkdir -p "${DIR}/security/tmp/"
+
+# Generate a dockerfile for each image
+image_counter=0
+for IMAGE_NAME in "${IMAGE_NAMES[@]}"; do
+  sed "s/{{IMAGE_NAME}}/${IMAGE_NAME//\//\/}/g" "${DIR}/security/Dockerfile-security.tmpl" > "${DIR}/security/tmp/Dockerfile-${SHORT_IMAGE_NAMES[$image_counter]}"
+  ((image_counter=image_counter+1))
+done
+
+# Update docker-compose.yml to build the security tag
+perl -p0e "s/(\s+)build:.+?(image:\s*quay\.io\/continuouspipe\/(.+?):)latest/\1build:\1  context: .\1  dockerfile: Dockerfile-\3\1\2security-$(date +%Y-%m-%d)/sg" "${DIR}/docker-compose.yml" > "${DIR}/security/tmp/docker-compose.yml"
+
+echo "Building all images:"; echo
+(cd "$DIR/security/tmp/" && docker-compose build --pull --force-rm)
+
+read -r -p "Would you like to publish the images? [Y/n] " DO_PUBLISH
+
+if [ -z "$DO_PUBLISH" ]; then
+  DO_PUBLISH='y'
+fi
+
+DO_PUBLISH="$(echo $DO_PUBLISH | tr '[:upper:]' '[:lower:]')"
+if [ "$DO_PUBLISH" = 'y' ]; then
+  echo "Pushing our images:"; echo
+  (cd "$DIR" && docker-compose push)
+else
+  echo "Not Pushing our images."; echo
+fi
+
+echo "Done!"; echo
