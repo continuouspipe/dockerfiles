ebe2dadfb205b7b537e4a4060ee554d8567e841a
Kieren Evans <kevans@inviqa.com>
2017-06-04 22:24:44 +0100

    Correct varnish backend host.

--- magento2-nginx/fpm-7.0/usr/local/share/magento2/magento_functions.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/magento2/magento_functions.sh
@@ -296,7 +296,7 @@ function do_replace_core_config_values() {
   INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'web/unsecure/base_url', '$PUBLIC_ADDRESS');
   INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'web/secure/base_url', '$PUBLIC_ADDRESS');
   INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'system/full_page_cache/varnish/access_list', 'varnish');
-  INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'system/full_page_cache/varnish/backend_host', 'varnish');
+  INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'system/full_page_cache/varnish/backend_host', 'web');
   INSERT INTO core_config_data VALUES (NULL, 'default', '0', 'system/full_page_cache/varnish/backend_port', '80');
   $ADDITIONAL_SETUP_SQL"
 
