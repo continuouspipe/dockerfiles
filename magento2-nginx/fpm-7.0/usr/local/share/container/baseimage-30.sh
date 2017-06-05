69e5b7d14edccada8806b232feba43c761398a8a
Kieren Evans <kevans@inviqa.com>
2017-06-05 22:01:06 +0100

    Allow choice of image version. 1, the default, will use the old setup to avoid breaking changes. 2 will use the new setup.

--- magento2-nginx/fpm-7.0/usr/local/share/container/baseimage-30.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/container/baseimage-30.sh
@@ -3,43 +3,75 @@
 alias_function do_build do_magento2_build_inner
 do_build() {
   do_magento2_build_inner
-  do_magento2_install
+  if [ "$IMAGE_VERSION" -ge 2 ]; then
+    do_magento2_build
+  else
+    do_magento2_install
+  fi
 }
 
-alias_function do_development_start do_magento2_development_start_inner
+if [ "$IMAGE_VERSION" -lt 2 ]; then
+  alias_function do_development_start do_magento2_development_start_inner
+fi
 do_development_start() {
-  do_magento2_development_start_inner
-  do_magento2_development_start
+  if [ "$IMAGE_VERSION" -ge 2 ]; then
+    do_php_nginx_development_start_inner
+    do_composer
+    do_magento2_development_build
+  else
+    do_magento2_development_start_inner
+    do_magento2_development_start
+  fi
 }
 
 alias_function do_templating do_magento2_templating_inner
 do_templating() {
-  mkdir -p /app/app/etc/
+  if [ "$IMAGE_VERSION" -ge 2 ]; then
+    do_magento2_templating
+  else
+    mkdir -p /app/app/etc/
+  fi
   do_magento2_templating_inner
 }
 
-do_composer() {
-  # disable original composer in image hierarchy till install_magento ported
-  :
-}
+if [ "$IMAGE_VERSION" -ge 2 ]; then
+  alias_function do_composer do_magento2_composer_inner
+  do_composer() {
+    do_composer_config
+    do_magento2_composer_inner
+    do_composer_post_install
+  }
+else
+  do_composer() {
+    # disable original composer in image hierarchy till install_magento ported
+    :
+  }
+fi
 
-do_magento2_install() {
-  bash /usr/local/share/magento2/install_magento.sh
-}
+if [ "$IMAGE_VERSION" -lt 2 ]; then
+  do_magento2_install() {
+    bash /usr/local/share/magento2/install_magento.sh
+  }
+fi
 
 do_setup() {
+  if [ "$IMAGE_VERSION" -ge 2 ]; then
+    do_templating
+  fi
   do_magento2_setup
 }
 
-do_magento2_setup() {
-  bash /usr/local/share/magento2/install_magento_finalise.sh
-}
+if [ "$IMAGE_VERSION" -lt 2 ]; then
+  do_magento2_setup() {
+    bash /usr/local/share/magento2/install_magento_finalise.sh
+  }
 
-do_magento2_development_start()
-{
-  bash /usr/local/share/magento2/development/install.sh
-  do_magento2_setup
-}
+  do_magento2_development_start()
+  {
+    bash /usr/local/share/magento2/development/install.sh
+    do_magento2_setup
+  }
+fi
 
 do_magento()
 {
@@ -50,3 +82,7 @@ do_magento()
     as_app_user "./bin/magento"
   fi
 }
+
+if [ "$IMAGE_VERSION" -ge 2 ]; then
+  source /usr/local/share/magento2/magento_functions.sh
+fi
