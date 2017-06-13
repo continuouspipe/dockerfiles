6e1d23d41a566665cafbd1692d7152ecee514bbb
Kieren Evans <kevans@inviqa.com>
2017-06-13 18:28:21 +0100

    Replace baseimage-40.sh's functions wholesale when IMAGE_VERSION is 2 or more.

--- magento2-nginx/fpm-7.0/usr/local/share/container/baseimage-30.sh
+++ magento2-nginx/fpm-7.0/usr/local/share/container/baseimage-30.sh
@@ -1,80 +1,81 @@
 #!/bin/bash
 
-alias_function do_build do_magento2_build_inner
-do_build() {
-  do_magento2_build_inner
-  if [ "$IMAGE_VERSION" -ge 2 ]; then
-    do_magento2_build
-  else
-    do_magento2_install
-  fi
-}
+if [ "$IMAGE_VERSION" -ge 2 ]; then
+  source /usr/local/share/magento2/magento_functions.sh
 
-if [ "$IMAGE_VERSION" -lt 2 ]; then
-  alias_function do_development_start do_magento2_development_start_inner
-fi
-do_development_start() {
-  if [ "$IMAGE_VERSION" -ge 2 ]; then
+  alias_function do_build do_magento2_build_inner
+  do_build() {
+    do_magento2_build_inner
+    do_magento2_build
+  }
+
+  do_development_start() {
     do_php_nginx_development_start_inner
     do_composer
     do_magento2_development_build
-  else
-    do_magento2_development_start_inner
-    do_magento2_development_start
-  fi
-}
+  }
 
-alias_function do_templating do_magento2_templating_inner
-do_templating() {
-  if [ "$IMAGE_VERSION" -ge 2 ]; then
+  alias_function do_templating do_magento2_templating_inner
+  do_templating() {
     do_magento2_templating
-  else
-    mkdir -p /app/app/etc/
-  fi
-  do_magento2_templating_inner
-}
+    do_magento2_templating_inner
+  }
 
-if [ "$IMAGE_VERSION" -ge 2 ]; then
   alias_function do_composer do_magento2_composer_inner
   do_composer() {
     do_composer_config
     do_magento2_composer_inner
     do_composer_post_install
   }
+
+  do_setup() {
+    do_templating
+    do_magento2_setup
+  }
 else
+  alias_function do_build do_magento2_build_inner
+  do_build() {
+    do_magento2_build_inner
+    do_magento2_install
+  }
+
+  alias_function do_development_start do_magento2_development_start_inner
+  do_development_start() {
+    do_magento2_development_start_inner
+    do_magento2_development_start
+  }
+
+  alias_function do_templating do_magento2_templating_inner
+  do_templating() {
+    mkdir -p /app/app/etc/
+    do_magento2_templating_inner
+  }
+
   do_composer() {
     # disable original composer in image hierarchy till install_magento ported
     :
   }
-fi
 
-if [ "$IMAGE_VERSION" -lt 2 ]; then
   do_magento2_install() {
     bash /usr/local/share/magento2/install_magento.sh
   }
-fi
 
-do_setup() {
-  if [ "$IMAGE_VERSION" -ge 2 ]; then
-    do_templating
-  fi
-  do_magento2_setup
-}
+  do_setup() {
+    do_magento2_setup
+  }
 
-if [ "$IMAGE_VERSION" -lt 2 ]; then
   do_magento2_setup() {
     bash /usr/local/share/magento2/install_magento_finalise.sh
   }
 
-  do_magento2_development_start()
-  {
+  do_magento2_development_start() {
     bash /usr/local/share/magento2/development/install.sh
     do_magento2_setup
   }
 fi
 
-do_magento()
-{
+
+do_magento() {
   set +x
   if [ "$#" -gt 0 ]; then
     as_app_user "./bin/magento $(printf "%q " "$@")"
@@ -82,7 +83,3 @@ do_magento()
     as_app_user "./bin/magento"
   fi
 }
-
-if [ "$IMAGE_VERSION" -ge 2 ]; then
-  source /usr/local/share/magento2/magento_functions.sh
-fi
