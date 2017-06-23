0fc0b7c3d044e0d1bd876c84f8f998bc7cea0fae
Kieren Evans <kevans@inviqa.com>
2017-06-23 09:42:43 +0100

    Fix where if a service is called "http" or "smtp", docker/kubernetes provides the IP address of the linked service as "tcp://<ip>:<port>"

--- mailcatcher/usr/local/bin/mailcatcher.sh
+++ mailcatcher/usr/local/bin/mailcatcher.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-export HTTP_PORT=${HTTP_PORT:-1080}
-export SMTP_PORT=${SMTP_PORT:-1025}
+HTTP_PORT=${HTTP_PORT:-1080}
+SMTP_PORT=${SMTP_PORT:-1025}
+if [[ "$HTTP_PORT" =~ "tcp://" ]]; then
+  HTTP_PORT="1080"
+fi
+if [[ "$SMTP_PORT" =~ "tcp://" ]]; then
+  SMTP_PORT="1025"
+fi
+export HTTP_PORT
+export SMTP_PORT
 
 exec mailcatcher --foreground --ip=0.0.0.0 "--smtp-port=$SMTP_PORT" "--http-port=$HTTP_PORT" --no-quit
