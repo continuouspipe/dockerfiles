2e7cafc9d352b472b5b584e80aae779cc57f503f
Samuel ROZE <samuel.roze@gmail.com>
2017-07-24 12:51:16 +0100

    Add support for the Doctrine migrations

--- symfony-flex/usr/local/share/symfony/symfony_flex_functions.sh
+++ symfony-flex/usr/local/share/symfony/symfony_flex_functions.sh
@@ -13,3 +13,24 @@ do_symfony_flex_encore_assets() {
     fi
   fi
 }
+
+do_migrate() {
+  if [ ! -f "bin/console" ]; then
+    echo "No console, will not run migrations"
+    return
+  fi
+
+  HAS_DOCTRINE_MIGRATIONS=`has_package doctrine/doctrine-migrations-bundle`
+  if [ "$HAS_DOCTRINE_MIGRATIONS" = "true" ]; then
+    bin/console doctrine:migrations:migrate
+  fi
+
+  HAS_DOCTRINE_ORM=`has_package doctrine/orm`
+  if [ "$HAS_DOCTRINE_ORM" = "true" ]; then
+    bin/console doctrine:schema:update --force
+  fi 
+}
+
+has_package() {
+  jq -c '.packages[] | select(.name == "'$1'") | has("name")' composer.lock
+}
