5aa4f9da35b45153af1da97f405a434dc1928b22
Andy Thompson <athompson@inviqa.com>
2017-08-08 09:42:42 +0100

    Make spryker image from php images and add apache
    Spryker doesn't use symfony, just symfony components, so the symfony image is mostly redundant.
    
    Also no reason not to have a Apache image

---
+++ spryker/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -xe
+
+if [ -L "$0" ] ; then
+    DIR="$(dirname "$(readlink -f "$0")")" ;
+else
+    DIR="$(dirname "$0")" ;
+fi ;
+
+for WEB_SERVER in nginx apache; do
+    # shellcheck disable=SC2043
+    for PHP_VERSION in 7.1; do
+        sed "s/{{PHP_VERSION}}/$PHP_VERSION/g; s/{{WEB_SERVER}}/$WEB_SERVER/g;" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-php${PHP_VERSION}-${WEB_SERVER}"
+    done
+done
