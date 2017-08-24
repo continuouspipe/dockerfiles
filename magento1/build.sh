f8eda65c5e3b35e94385e7bf43eb52727f863061
Andy Thompson <athompson@inviqa.com>
2017-08-24 16:00:26 +0100

    Create a PHP 5.5 Magento1 EOL image for older Magento versions not patched
    Don't create a PHP 5.5 image directly as most frameworks support 5.6 upwards

--- magento1/build.sh
+++ magento1/build.sh
@@ -13,4 +13,10 @@ for WEB_SERVER in nginx apache; do
     for PHP_VERSION in 5.6; do
         sed "s/{{PHP_VERSION}}/$PHP_VERSION/g; s/{{WEB_SERVER}}/$WEB_SERVER/g;" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-php${PHP_VERSION}-${WEB_SERVER}"
     done
+
 done
+
+cat "$DIR/../php/Dockerfile-nginx-phpsource" > "$DIR/Dockerfile-eol-php5.5-nginx"
+# shellcheck disable=SC2016
+sed '1,/^MAINTAINER /d; s/{{PHP_VERSION}}/${PHP_VERSION}/g; s/{{WEB_SERVER}}/nginx/g;' "$DIR/Dockerfile.tmpl" >> "$DIR/Dockerfile-eol-php5.5-nginx"
+
