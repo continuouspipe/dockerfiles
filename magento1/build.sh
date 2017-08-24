4d688514eb693b7b32988992d72f77276f18363a
Andy Thompson <athompson@inviqa.com>
2017-08-24 16:45:43 +0100

    Create PHP 5.5 eol image
    It's not otherwise easy to include the COPY commands from it

--- magento1/build.sh
+++ magento1/build.sh
@@ -11,12 +11,14 @@ fi ;
 for WEB_SERVER in nginx apache; do
     # shellcheck disable=SC2043
     for PHP_VERSION in 5.6; do
-        sed "s/{{PHP_VERSION}}/$PHP_VERSION/g; s/{{WEB_SERVER}}/$WEB_SERVER/g;" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-php${PHP_VERSION}-${WEB_SERVER}"
+        sed "s/{{IMAGE_NAME}}/php${PHP_VERSION}-${WEB_SERVER}/g; s/{{PHP_VERSION}}/$PHP_VERSION/g; s/{{WEB_SERVER}}/$WEB_SERVER/g;" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-php${PHP_VERSION}-${WEB_SERVER}"
     done
-
 done
 
-cat "$DIR/../php/Dockerfile-nginx-phpsource" > "$DIR/Dockerfile-eol-php5.5-nginx"
-# shellcheck disable=SC2016
-sed '1,/^MAINTAINER /d; s/{{PHP_VERSION}}/${PHP_VERSION}/g; s/{{WEB_SERVER}}/nginx/g;' "$DIR/Dockerfile.tmpl" >> "$DIR/Dockerfile-eol-php5.5-nginx"
-
+# shellcheck disable=SC2043
+for WEB_SERVER in nginx; do
+    # shellcheck disable=SC2043
+    for PHP_VERSION in 5.5; do
+        sed "s/{{IMAGE_NAME}}/eol-php${PHP_VERSION}-${WEB_SERVER}/g;s/{{PHP_VERSION}}/$PHP_VERSION/g; s/{{WEB_SERVER}}/$WEB_SERVER/g;" "${DIR}/Dockerfile.tmpl" > "${DIR}/Dockerfile-eol-php${PHP_VERSION}-${WEB_SERVER}"
+    done
+done
