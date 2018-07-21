68dd518c330fc45fa4b2ad9b13104c15622d83f6
Kieren Evans <kevans@inviqa.com>
2018-07-21 12:25:45 +0300

    Spryker: Turn off Xdebug during development_start

--- spryker/usr/local/share/container/baseimage-45.sh
+++ spryker/usr/local/share/container/baseimage-45.sh
@@ -22,10 +22,17 @@ do_start() {
 
 alias_function do_development_start do_spryker_development_start_inner
 do_development_start() {
+  local OLD_XDEBUG_REMOTE_ENABLED="$XDEBUG_REMOTE_ENABLED"
+  if is_true "$OLD_XDEBUG_REMOTE_ENABLED"; then
+    XDEBUG_REMOTE_ENABLED=false do_templating
+  fi
   do_spryker_development_start_inner
   do_spryker_build
   do_spryker_install
   do_spryker_app_permissions
+  if is_true "$OLD_XDEBUG_REMOTE_ENABLED"; then
+    do_templating
+  fi
 }
 
 alias_function do_setup do_spryker_setup_inner
