23c7a8d34ecd0f77938f8ad9dfbb64fe7ea6bd8c
Kieren Evans <kevans@inviqa.com>
2018-07-21 11:26:28 +0300

    Add server name

--- php/tests/integration/tests/plan.sh
+++ php/tests/integration/tests/plan.sh
@@ -4,16 +4,18 @@ do_run_tests()
 {
   set -o pipefail
   local SERVICE_NAME
-  for version in 7.2 7.1 5.6; do
-    SERVICE_NAME="web_${version/./_}"
-    wait_for_remote_ports 60 "$SERVICE_NAME:443"
-    curl --insecure --fail "https://$SERVICE_NAME/" | grep PHP_VERSION | grep -q "$version"
-  done
+  for server in nginx apache; do
+    for version in 7.2 7.1 5.6; do
+      SERVICE_NAME="web_${version/./_}_${server}"
+      wait_for_remote_ports 60 "$SERVICE_NAME:443"
+      curl --insecure --fail "https://$SERVICE_NAME/" | grep PHP_VERSION | grep -q "$version"
+    done
 
-  # shellcheck disable=SC2043
-  for version in 7; do
-    SERVICE_NAME="web_${version}_0"
-    wait_for_remote_ports 60 "$SERVICE_NAME:443"
-    curl --insecure --fail "https://$SERVICE_NAME/" | grep PHP_VERSION | grep -q "${version}\.0"
+    # shellcheck disable=SC2043
+    for version in 7; do
+      SERVICE_NAME="web_${version}_0_${server}"
+      wait_for_remote_ports 60 "$SERVICE_NAME:443"
+      curl --insecure --fail "https://$SERVICE_NAME/" | grep PHP_VERSION | grep -q "${version}\.0"
+    done
   done
 }
