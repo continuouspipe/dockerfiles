dc2c1cef734c47da4f6a3527690a7cfc8da32d2d
Kieren Evans <kevans@inviqa.com>
2018-10-20 19:20:32 +0300

    Speed up test script to make way for image publishing on master

--- test.sh
+++ test.sh
@@ -11,26 +11,42 @@ fi
 docker pull koalaman/shellcheck:v0.4.6
 docker pull lukasmartinelli/hadolint
 
-find "$DIR" -type f ! -path "*.git/*" ! -name "*.py" \( \
-  -perm +111 -or -name "*.sh" -or -wholename "*usr/local/share/env/*" -or -wholename "*usr/local/share/container/*" \
-\) | while read -r script; do
+run_shellcheck()
+{
+  local script="$1"
   echo "Linting '$script':";
   docker run --rm -i koalaman/shellcheck:v0.4.6 --exclude SC1091 - < "$script";
   echo
-done
+}
+export -f run_shellcheck
 
-find "$DIR" -type f -name "Dockerfile*" ! -name "*.tmpl" | while read -r dockerfile; do
+find "$DIR" -type f ! -path "*.git/*" ! -name "*.py" \( \
+  -perm +111 -or -name "*.sh" -or -wholename "*usr/local/share/env/*" -or -wholename "*usr/local/share/container/*" \
+\) | parallel run_shellcheck
+
+run_hadolint()
+{
+  local dockerfile="$1"
   echo "Linting '$dockerfile':";
   docker run --rm -i lukasmartinelli/hadolint hadolint --ignore DL3008 --ignore DL3002 --ignore DL3003 --ignore DL4001 --ignore DL3007 --ignore SC2016 - < "$dockerfile"
   echo
-done
+}
+export -f run_hadolint
+
+find "$DIR" -type f -name "Dockerfile*" ! -name "*.tmpl" | parallel run_hadolint
 
 # Run unit tests
 docker-compose -f docker-compose.yml -f docker-compose.test.yml run --rm tests
 
-# Run integration tests
-find "$DIR" -type f -path "*/tests/integration/docker-compose.yml" | while read -r integration_docker_compose; do
+run_integration_tests()
+{
+  local integration_docker_compose="$1"
   echo "Running integration tests for '$integration_docker_compose':"
-  docker-compose -f "$DIR/docker-compose.yml" -f "$integration_docker_compose" up --build --exit-code-from integration_tests integration_tests;
-  docker-compose -f "$DIR/docker-compose.yml" -f "$integration_docker_compose" down -v --rmi local
-done
+  docker-compose -f "$DIR/docker-compose.yml" -f "$integration_docker_compose" build --parallel integration_tests
+  docker-compose -f "$DIR/docker-compose.yml" -f "$integration_docker_compose" up --exit-code-from integration_tests integration_tests
+  docker-compose -f "$DIR/docker-compose.yml" -f "$integration_docker_compose" down -v local
+}
+export -f run_integration_tests
+
+# Run integration tests
+find "$DIR" -type f -path "*/tests/integration/docker-compose.yml" | parallel run_integration_tests
