cb49cda547dff57450401144a50040c7ff04d6ed
Kieren Evans <kevans@inviqa.com>
2019-07-17 21:42:52 +0300

    Add integration test for PHP 7.3

--- php/tests/integration/tests/plan.sh
+++ php/tests/integration/tests/plan.sh
@@ -5,7 +5,7 @@ do_run_tests()
   set -o pipefail
   local SERVICE_NAME
   for server in nginx apache; do
-    for version in 7.2 7.1 5.6; do
+    for version in 7.3 7.2 7.1 5.6; do
       SERVICE_NAME="web_${version/./_}_${server}"
       wait_for_remote_ports 60 "$SERVICE_NAME:443"
       curl --insecure --fail "https://$SERVICE_NAME/" | grep PHP_VERSION | grep -q "$version"
