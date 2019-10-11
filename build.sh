a9893fd9ebba54d9fe2e396735bf2a74b349d2bd
Kieren Evans <kevans@inviqa.com>
2019-10-11 20:25:00 +0300

    Build magento 2 PHP 7.3 image

--- build.sh
+++ build.sh
@@ -132,6 +132,7 @@ variables()
       'magento2_php71_nginx'
       'magento2_php71_nginx_ng'
       'magento2_php72_nginx_ng'
+      'magento2_php73_nginx_ng'
       'magento2_varnish'
       'mailcatcher'
       'memcached'
